0d303e8a43dec2b8b008d6afca88f2a527fa5954
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-09 13:52:47 +0200

    avancer des exercice avec fin exo 9 attente avant correction

--- src/exo8ResolutionEquationSecondDegres.java
+++ src/exo8ResolutionEquationSecondDegres.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 public class exo8ResolutionEquationSecondDegres {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Saisir a");
+        System.out.print("Saisir a : ");
         float a = input.nextFloat();
-        System.out.print("Saisir b");
+        System.out.print("Saisir b : ");
         float b = input.nextFloat();
-        System.out.print("Saisir c");
+        System.out.print("Saisir c : ");
         float c = input.nextFloat();
-        float delta;
+        double delta;
         delta = Math.pow(b,2)-4*a*c;
         if (delta<0)
         {
             System.out.println("Il n'y a pas de valeur réelle");
         }
-        if (delta=0)
+        else if (delta==0)
         {
-            float x0 = (-b)/2*a;
-            System.out.printf("L'unique solution est %f",x0);
+            double x0 = (-b)/(2*a);
+            System.out.printf("Le discriminant vaut %f L'unique solution est %f",delta,x0);
         }
         else
         {
-            Double x1 = (-b)-(Math.sqrt(delta)/(2*a));
-            Double x2 = (-b)-(Math.sqrt(delta)/(2*a));
-            System.out.printf("Les solutions sont %f et %f",x1,x2);
+            double x1 = ((-b)-(Math.sqrt(delta)))/(2*a);
+            double x2 = ((-b)+(Math.sqrt(delta)))/(2*a);
+            System.out.printf("Le discriminant vaut %f Les solutions sont %f et %f",delta,x1,x2);
         }
 
     }
---
+++ src/exo9AfficherLeMoisCorrespondant.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class exo9AfficherLeMoisCorrespondant {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Afficher le numero du mois : ");
+        int mois = input.nextInt();
+        switch (mois)
+        {
+            case 1:
+            {
+                System.out.println("Janvier");
+                break;
+            }
+            case 2:
+            {
+                System.out.println("Février");
+                break;
+            }
+            case 3:
+            {
+                System.out.println("Mars");
+                break;
+            }
+            case 4:
+            {
+                System.out.println("Avril");
+                break;
+            }
+            case 5:
+            {
+                System.out.println("Mai");
+                break;
+            }
+            case 6:
+            {
+                System.out.println("Juin");
+                break;
+            }
+            case 7:
+            {
+                System.out.println("Juillet");
+                break;
+            }
+            case 8:
+            {
+                System.out.println("Aout");
+                break;
+            }
+            case 9:
+            {
+                System.out.println("Septembre");
+                break;
+            }
+            case 10:
+            {
+                System.out.println("Octobre");
+                break;
+            }
+            case 11:
+            {
+                System.out.println("Novembre");
+                break;
+            }
+            case 12:
+            {
+                System.out.println("Decembre");
+                break;
+            }
+            default:
+            {
+                System.out.println("Votre numero n'est pas compris entre 1 et 12, donc aucun mois lui est attribue");
+            }
+        }
+
+    }
+}
