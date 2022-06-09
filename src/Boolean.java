913c5f4addcb122afec61831c89bc6d0b87b73b6
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-09 09:50:57 +0200

    boolean cours

---
+++ src/Boolean.java
@@ -0,0 +1,24 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean isMarried=false;
+        if (isMarried)
+        {
+            System.out.println("You are married");
+        }
+        else
+        {
+            System.out.println("You are not married");
+        }
+
+        byte age = 18;
+        boolean estMajeur=age>=18;
+        if (estMajeur)
+        {
+            System.out.println("Tu es majeur");
+        }
+        else
+        {
+            System.out.println("Tu es mineur");
+        }
+    }
+}
