42d32a0f60e0eff7d337f83dd46b815634d2fbb6
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-09 12:00:41 +0200

    déclaration avec var

---
+++ src/FonctionMaths.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class FonctionMaths {
+    public static void main(String[] args) {
+        double nombre=81;
+        double nombre2=25;
+
+        //Calculer la racine carre de 81
+        System.out.println(Math.sqrt(nombre));
+        //Elever 25 a la puissance 3
+        System.out.println(Math.pow(nombre2,3));
+        //
+    }
+}
