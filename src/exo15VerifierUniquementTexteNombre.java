f80bcd4bea21bf4fdaad45e8955f89a2138dab3a
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-11 09:41:15 +0200

    Exo 15 autre test pour vérifier si les caractères sont des nombres

--- src/exo15VerifierUniquementTexteNombre.java
+++ src/exo15VerifierUniquementTexteNombre.java
@@ -2,36 +2,24 @@ import java.util.Scanner;
 public class exo15VerifierUniquementTexteNombre {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int o = 0;
         System.out.print("Entrez votre code PIN : ");
         String codePIN = input.nextLine();
-        while (true)
+        for (int i = 0; i <= codePIN.length()-1; i++)
         {
-            for (int i = 0; i <= codePIN.length()-1; i++)
+            if (Character.isDigit(codePIN.charAt(i)))
             {
-                if (Character.isDigit(codePIN.charAt(i)) == true)
-                {
-                    o++;
-                }
-                else if (Character.isDigit(codePIN.charAt(i)) == false)
-                {
-                    o = 0;
-                    System.out.println("Vous avez saisi une lettre, recommencez avec uniquement des nombres");
-                }
-                if (o == codePIN.length())
-                {
-                    System.out.println("Vous avez saisi que des nombres");
-                    System.exit(1);
-                }
+                    continue;
             }
-            codePIN = input.nextLine();
+            if (!Character.isDigit(codePIN.charAt(i)))
+            {
+                i=-1;
+                System.out.println("Vous avez saisi une lettre, recommencez avec uniquement des nombres");
+                codePIN = input.nextLine();
+            }
         }
-
-
-
-
-
-
-
+        System.out.println("Vous avez saisi que des nombres");
+        System.exit(1);
     }
+
 }
+
