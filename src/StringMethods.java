848754cca3d324f22ea8a5e4a6a5349484187b49
Steven Dumortier <stevendumortier59@gmail.com>
2022-06-13 11:53:36 +0200

    Cours string, tableau mémoire, methodestring

---
+++ src/StringMethods.java
@@ -0,0 +1,26 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        //afficher ou selectionner un caractère d'un string
+        String nom="Maeva";
+        System.out.println(nom.charAt(2));
+
+        // Parcourir d'un string caractère par caractère
+        System.out.println("Itération sur les charactères\n ==========================");
+        for(int i=0;i<nom.length();i++)
+        {
+            System.out.println(nom.charAt(i));
+        }
+        // variable.intexOf() : Retourn la première occurence de l'index du caractère passé en argument
+        System.out.println(nom.indexOf('a'));
+
+        //remplacement des caractères
+        String nom2="abracadabra";
+        System.out.println(nom2.replace('a','*'));
+
+        // Extraire une sous-chaine : Christian Lisangola
+        String chaine = "Christian Lisangola";
+        String chaine2 = chaine.substring(0,9); //[0;9[
+        System.out.println(chaine2);
+
+    }
+}
